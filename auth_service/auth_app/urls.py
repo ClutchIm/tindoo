98dc6e74d2a179f45757e268844408dba41b310f
ClutchIm <clutcher.sorry@gmail.com>
2025-03-18 19:34:32 +0300

    0.1.3 | Auth_service: A little bit fix in base user view endpoint

--- auth_service/auth_app/urls.py
+++ auth_service/auth_app/urls.py
@@ -14,7 +14,7 @@ router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='user')
 
 urlpatterns = [
-    path('api/', include(router.urls)),
+    path('api/users/', include(router.urls)),
     path('api/register/', RegisterView.as_view(), name='register'),
     path('api/verify-email/', VerifyEmailView.as_view(), name='verify-email'),
     path('api/resend-verification/', ResendVerificationCodeView.as_view(), name='resend-verification'),
